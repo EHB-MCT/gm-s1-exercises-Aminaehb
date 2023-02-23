01ed513153d14c439397c5c6a1c719d6b5775d23
Aminaehb <71768425+Aminaehb@users.noreply.github.com>
2023-02-23 16:21:12 +0100

    exercise 2 done

--- ex-2.js
+++ ex-2.js
@@ -4,14 +4,36 @@
 //Check https://p5js.org/reference/ on how to access mouse data
 
 let w, h;
+//let loc;
 
 function setup() {
     w = windowWidth;
     h = windowHeight;
+
+    // loc = createVector(windowWidth / 2, windowHeight / 2); mag maar locatie wordt niet bijgehouden
     createCanvas(w, h);
+    //createCanvas(windowWidth, windowHeight);
     background(200);
 }
 
 function draw() {
+    let center = createVector(w / 2, h / 2);
+    let mouse = createVector(mouseX, mouseY);
+
     background(200);
+
+
+    //line(mouseX, 0, mouseX, 100);
+    //line (0, 0, center.x, center.y)
+    line (0, 0, mouse.x, mouse.y)
+
+
+    mouse.sub(center);
+    
+    translate(w/2, h/2);
+    line (0, 0, mouse.x, mouse.y)
+
+    
+
+    //describe('horizontal black line moves left and right with mouse x-position');
 }
